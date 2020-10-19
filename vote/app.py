bd570630199a8fe14440f73f75eb527521bd4f8e
Anirudh Raghunath <anraghun@github.com>
2020-10-19 19:34:11 +0530

    added option env vars

--- vote/app.py
+++ vote/app.py
@@ -2,6 +2,7 @@ import os
 import random
 import json
 import socket
+import sys
 from flask import Flask, render_template, request, make_response, g
 from azure.storage.queue import QueueService, QueueMessageFormat
 
@@ -28,11 +29,14 @@ def home():
 
     vote = None
 
-    if request.method == 'POST':
-        queue = get_queue()
-        vote = request.form['vote']
-        data = json.dumps({'voter_id': voter_id, 'vote': vote})
-        queue.put_message('votes', unicode(data))
+    try:
+        if request.method == 'POST':
+            queue = get_queue()
+            vote = request.form['vote']
+            data = json.dumps({'voter_id': voter_id, 'vote': vote})
+            queue.put_message('votes', unicode(data))
+    except:
+        raise Exception(sys.exc_info()[0])
 
     resp = make_response(render_template(
         'index.html',
